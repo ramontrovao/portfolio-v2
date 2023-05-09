208dc740ca2c310ed95277a4b52361a7b4ac43f1
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-05-08 23:33:49 -0300

    seo: added informational meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,35 @@ const Home = () => {
       <Head>
         <title>Ramon Pinheiro | Desenvolvedor de software</title>
         <link rel="icon" type="image/x-icon" href="/assets/logo.svg" />
+        <meta name="title" content="Ramon Pinheiro" />
+        <meta
+          name="description"
+          content="Sou desenvolvedor mobile fullstack e trabalho com as melhores tecnologias do mercado."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ramontrovao.tech/" />
+        <meta property="og:title" content="Ramon Pinheiro" />
+        <meta
+          property="og:description"
+          content="Sou desenvolvedor mobile fullstack e trabalho com as melhores tecnologias do mercado."
+        />
+        <meta
+          property="og:image"
+          content="https://wallpaperaccess.com/full/201322.jpg"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://ramontrovao.tech" />
+        <meta property="twitter:title" content="Ramon Pinheiro" />
+        <meta
+          property="twitter:description"
+          content="Sou desenvolvedor mobile fullstack e trabalho com as melhores tecnologias do mercado."
+        />
+        <meta
+          property="twitter:image"
+          content="https://wallpaperaccess.com/full/201322.jpg"
+        />
       </Head>
 
       <MainSection />
